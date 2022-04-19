00a6b63b84b9810e19eac16229c0307365aaa01d
Reyuvajo <100845222+Reyuvajo@users.noreply.github.com>
2022-04-19 19:51:09 +0400

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,40 @@
+function play() {
+    var audio = document.getElementById('audio');
+    if (audio.paused) {
+        audio.play();
+    }
+    else {
+        audio.pause();
+        audio.currentTime = 0
+    }
+};
+function play1() {
+    var audi = document.getElementById('audi');
+    if (audi.paused) {
+        audi.play();
+    }
+    else {
+        audi.pause();
+        audi.currentTime = 0
+    }
+};
+function play2() {
+    var aud = document.getElementById('aud');
+    if (aud.paused) {
+        aud.play();
+    }
+    else {
+        aud.pause();
+        aud.currentTime = 0
+    }
+};
+function play3() {
+    var aud = document.getElementById('au');
+    if (au.paused) {
+        au.play();
+    }
+    else {
+        au.pause();
+        au.currentTime = 0
+    }
+};
